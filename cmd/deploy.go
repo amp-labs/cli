4fc48980019d2343afee3188dcaeffdc61de1b96
Lauren Long <laurenzlong@noreply.github.com>
2023-02-10 13:46:49 -0800

    Add deploy command

---
+++ cmd/deploy.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// deployCmd represents the deploy command
+var deployCmd = &cobra.Command{
+	Use:   "deploy",
+	Short: "Deploy amp.yaml file",
+	Long: `Deploy changes to amp.yaml file.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("deploy called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(deployCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// deployCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// deployCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
