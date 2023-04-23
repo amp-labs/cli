914852d91557c59be7e8c5d118074cdd27e86d8c
Rishabh2991 <bhardwajrishabh76@gmail.com>
2023-04-24 01:14:23 +0530

    fix indents

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -1,16 +1,16 @@
 package cmd
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"path/filepath"
-	"time"
-
-	"github.com/amp-labs/cli/cmd/helpers/upload"
-	"github.com/amp-labs/cli/cmd/helpers/util"
-	"github.com/amp-labs/cli/cmd/helpers/zip"
-	"github.com/spf13/cobra"
+  "fmt"
+  "log"
+  "os"
+  "path/filepath"
+  "time"
+  
+  "github.com/amp-labs/cli/cmd/helpers/upload"
+  "github.com/amp-labs/cli/cmd/helpers/util"
+  "github.com/amp-labs/cli/cmd/helpers/zip"
+  "github.com/spf13/cobra"
 )
 
 var now = time.Now()
@@ -20,70 +20,65 @@ var filePath = "cmd/amp/amp.yaml"
 
 // deployCmd represents the deploy command
 var deployCmd = &cobra.Command{
-	Use:   "deploy",
-	Short: "Deploy amp.yaml file",
-	Long:  `Deploy changes to amp.yaml file.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("deploy called")
-		fmt.Println("Creating a local copy of the AMP file")
-
-		//Resolve amp.yaml's location using current working directory path and constant directory extension
-		workingDir,_  := os.Getwd()
-		var filename = filepath.ToSlash(filepath.Join(workingDir,filePath))
-
-		//make a local copy of the amp.yaml by attaching a timestamp value to its name
-
-		var localVersion = fmt.Sprintf("amp_%d.yaml", now.Unix())
-		_copied := util.Copy(filename,localVersion)
-
-		if _copied{
-			fmt.Println("zipping file name:",localVersion)
-			
-			var zipPath = fmt.Sprintf("amp_%d.zip", now.Unix())
-			
-			_zipped := zip.Zip(localVersion, zipPath)
-			
-			if _zipped{
-				fmt.Println("Uploading zipped file to storage bucket")
-				uploadPath,uploadErrors = upload.Upload(zipPath)
-			}else {
-				fmt.Println("Exiting following unsuccesful zip operation")
-				
-				return
-			}
-			fmt.Println("Deleting local amp zipped file")
-			
-			clean_up(zipPath)
-		}
-		
-		clean_up(localVersion)
-
-		if uploadErrors!=nil{
-			fmt.Println("Exiting following unsuccesful upload operation")
-		}else{
-			fmt.Println("Succesfully uploaded zipped file to remote storage..",uploadPath)
-		}
-	},
+  Use:   "deploy",
+  Short: "Deploy amp.yaml file",
+  Long:  `Deploy changes to amp.yaml file.`,
+  Run: func(cmd *cobra.Command, args []string) {
+    fmt.Println("deploy called")
+    fmt.Println("Creating a local copy of the AMP file")
+    
+    //Resolve amp.yaml's location using current working directory path and constant directory extension
+    workingDir,_  := os.Getwd()
+    var filename = filepath.ToSlash(filepath.Join(workingDir,filePath))
+    
+    //make a local copy of the amp.yaml by attaching a timestamp value to its name
+    
+    var localVersion = fmt.Sprintf("amp_%d.yaml", now.Unix())
+    _copied := util.Copy(filename,localVersion)
+    
+    if _copied{
+      fmt.Println("zipping file name:",localVersion)
+      var zipPath = fmt.Sprintf("amp_%d.zip", now.Unix())
+      _zipped := zip.Zip(localVersion, zipPath)
+      
+      if _zipped{
+        fmt.Println("Uploading zipped file to storage bucket")
+        uploadPath,uploadErrors = upload.Upload(zipPath)
+        }else {
+          fmt.Println("Exiting following unsuccesful zip operation")
+          return
+        }
+        fmt.Println("Deleting local amp zipped file")
+        clean_up(zipPath)
+    }
+    
+    clean_up(localVersion)
+    
+    if uploadErrors!=nil{
+      fmt.Println("Exiting following unsuccesful upload operation")
+      }else{
+        fmt.Println("Succesfully uploaded zipped file to remote storage..",uploadPath)
+      }
+  },
 }
 
 func init() {
-	rootCmd.AddCommand(deployCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// deployCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// deployCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+  rootCmd.AddCommand(deployCmd)
+  
+  // Here you will define your flags and configuration settings.
+  // Cobra supports Persistent Flags which will work for this command
+  // and all subcommands, e.g.:
+  // deployCmd.PersistentFlags().String("foo", "", "A help for foo")
+  
+  // Cobra supports local flags which will only run when this command
+  // is called directly, e.g.:
+  // deployCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func clean_up(filename string){
-	err := os.Remove(filename)
-	if err != nil{
-		log.Fatal(err)
-		return
-	}
+  err := os.Remove(filename)
+  if err != nil{
+    log.Fatal(err)
+    return
+  }
 }
--- cmd/helpers/util/util.go
+++ cmd/helpers/util/util.go
@@ -8,38 +8,33 @@ import (
 
 var errorkey = "ERROR:Ampersand-Cli:cli/helpers/utils/Copy: "
 
-func Copy(sourcefile,localcopy string) bool {
+func Copy(sourcefile, localcopy string) bool {
 
 	srcFile, err := os.Open(sourcefile)
-	if err!=nil{
+	if err != nil {
 		log.Fatal(errorkey, err)
 		return false
-
 	}
-  defer srcFile.Close()
+	defer srcFile.Close()
 
 	destFile, err := os.Create(localcopy) // creates if file doesn't exist
-    
-	if err!=nil{
+
+	if err != nil {
 		log.Fatal(errorkey, err)
 		return false
 	}
-	
 	defer destFile.Close()
-	
+
 	_, err = io.Copy(destFile, srcFile)
-  if err!=nil{
+	if err != nil {
 		log.Fatal(errorkey, err)
 		return false
 	}
 
-    err = destFile.Sync()
-    if err!=nil{
-			log.Fatal(errorkey, err)
+	err = destFile.Sync()
+	if err != nil {
+		log.Fatal(errorkey, err)
 		return false
-
 	}
-
 	return true
-
 }
