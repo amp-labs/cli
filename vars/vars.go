5d6326fc6164032bfbff5a848e260f7d80c21f00
Chris Eberle <eberle1080@gmail.com>
2023-07-27 11:58:03 -0700

    add comment

--- vars/vars.go
+++ vars/vars.go
@@ -1,5 +1,7 @@
 package vars
 
+// These variables are set at build time (using -ldflags -X ...)
+
 var (
 	ClerkRootURL = "https://welcomed-snapper-45.clerk.accounts.dev/"
 	LoginURL     = "https://ampersand-cli-auth-dev.web.app"
