0885cdc7faad0130d34d638ce1d3ee099615cc95
Rajat Pawar <18614743+RajatPawar@users.noreply.github.com>
2023-09-13 13:05:18 +0530

    update error checking

--- flags/config.go
+++ flags/config.go
@@ -1,6 +1,8 @@
 package flags
 
 import (
+	"log"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -13,7 +15,10 @@ var Config FlagConfig
 
 func Init(rootCmd *cobra.Command) {
 	rootCmd.PersistentFlags().BoolVarP(&Config.DebugMode, "debug", "d", false, "Enable debug logging mode")
-	viper.BindPFlag("debug", rootCmd.PersistentFlags().Lookup("debug"))
+	err := viper.BindPFlag("debug", rootCmd.PersistentFlags().Lookup("debug"))
+	if err != nil {
+		log.Fatal("unable to initialise flags: ", err)
+	}
 }
 
 func GetDebugMode() bool {
