8ea63f4faec50bb435496226cf2c1dc60f04b291
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-02-25 21:38:13 -0800

    [ENG-669] feat: add delete:installation command

---
+++ cmd/delete_installation.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"github.com/amp-labs/cli/flags"
+	"github.com/amp-labs/cli/logger"
+	"github.com/amp-labs/cli/request"
+	"github.com/spf13/cobra"
+)
+
+var deleteInstallationCmd = &cobra.Command{ //nolint:gochecknoglobals
+	Use:   "delete:installation <integrationId> <installationId>",
+	Short: "Delete installation",
+	Long:  "Delete installation",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		logger.Debug("Deleting installation")
+
+		integrationId := args[0]
+		installationId := args[1]
+		projectId := flags.GetProjectId()
+		if projectId == "" {
+			logger.Fatal("Must provide a project ID in the --project flag")
+		}
+
+		apiKey := flags.GetAPIKey()
+
+		err := request.NewAPIClient(projectId, &apiKey).
+			DeleteInstallation(cmd.Context(), integrationId, installationId)
+		if err != nil {
+			logger.FatalErr("Unable to delete installation", err)
+		}
+
+		logger.Info("Successfully deleted installation.")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(deleteInstallationCmd)
+}
