d5e864f406e2683b55d45e4f7bbd3546576ab991
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-02-25 21:46:36 -0800

    chore: ignore silly linter error

--- cmd/delete_installation.go
+++ cmd/delete_installation.go
@@ -11,7 +11,7 @@ var deleteInstallationCmd = &cobra.Command{ //nolint:gochecknoglobals
 	Use:   "delete:installation <integrationId> <installationId>",
 	Short: "Delete installation",
 	Long:  "Delete installation",
-	Args:  cobra.ExactArgs(2),
+	Args:  cobra.ExactArgs(2), //nolint:gomnd
 	Run: func(cmd *cobra.Command, args []string) {
 		logger.Debug("Deleting installation")
 
